df58dc42217f034bdfff23027029cfbc7de5b89c
jiyunhuang <jiyunhuang@tencent.com>
2020-05-04 21:52:14 +0800

    FEATURE: 增加了获取物料的接口

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from threading import Thread
 
 import grpc
 
+from pb.item import item_pb2_grpc
 from pb.recommand import recommand_pb2_grpc
+from pb_imp.item_imp import ItemService
 from pb_imp.recommand_imp import RecommandService
 from service.kafka_consumer import consume_user_action
 
@@ -12,6 +14,7 @@ if __name__ == '__main__':
     # 启动 rpc 服务
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     recommand_pb2_grpc.add_RecommandServiceServicer_to_server(RecommandService(), server)
+    item_pb2_grpc.add_ItemServiceServicer_to_server(ItemService(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     # 启动 kafka 消费者
